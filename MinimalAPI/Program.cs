cd2228a22646c6cb9e75da496590e0f2b9db03cd
arturoatt <arturoatt@gmail.com>
2025-02-17 10:58:32 -0600

    Add project files.

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,17 @@
+using MinimalAPI.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.MapEndpoints();
+
+app.Run();
+
+
